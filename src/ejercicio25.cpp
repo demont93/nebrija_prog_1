8167324014184ad3e255138bca0c985bc8de3c4e
demont93 <demont93@gmail.com>
2020-10-20 18:34:06 -0400

    Display message for ejercicio25.cpp

--- src/ejercicio25.cpp
+++ src/ejercicio25.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include "user_io.h"
 
 // Realizar un programa que pida al usuario un número y muestre por pantalla si
 // el número es primo o no.
@@ -7,5 +8,7 @@
 // Vea ejercicio19
 
 int main() {
+  UserIO io;
+  io << "Vea ejercicio19";
   return 0;
 }
